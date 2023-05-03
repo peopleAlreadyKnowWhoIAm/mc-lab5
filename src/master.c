95fb33890931f9a4f62361958e8169cf2da61b06
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2023-05-03 21:32:57 +0300

    Deleted commented code

--- src/master.c
+++ src/master.c
@@ -1,8 +1,6 @@
 #include "main.h"
 
 int main() {
-  // USART0->UCSRB |= _BV(TXEN0);
-  // USART0->UCSRB &= ~_BV(TXEN0);
 
   MCC* rs = MCCInit(USART1, RS485_BR, MCC_MASTER,
                     (Pin){.gpio = GPIOD, .pin_number = 4});
@@ -39,70 +37,7 @@ int main() {
       }
     }
 
-    // for (uint8_t i = 0; i < 5; i++) {
-    //   MCCMasterSendAddress(rs, 94);
-    //   while (RS485Busy(rs)) {
-    //   }
-    //   RS485Write(rs, "a");
-    //   while (RS485Busy(rs)) {
-    //   }
-
-    //   char* tbuf = MCCGetBuffer(rs);
-    //   uint8_t tlen = MCCGetDataLength(rs);
-    //   // RS485Read(rs, buf);
-    //   memcpy(buf, tbuf, tlen);
-
-    //   RS485MasterSendAddress(rs, 38);
-    //   while (RS485Busy(rs)) {
-    //   }
-    //   RS485Write(rs, "a");
-    //   while (RS485Busy(rs)) {
-    //   }
-    //   char a[100];
-    //   // RS485Read(rs, a);
-    //   char* sbuf = MCCGetBuffer(rs);
-    //   uint8_t slen = MCCGetDataLength(rs);
-    //   // tbuf.str[tbuf.len] = '\0';
-    //   // strcat(buf, a);
-    //   memcpy(&buf[tlen], sbuf, slen);
-
-    //   UsartWriteChars(usrt, buf, slen + tlen);
-    //   while (UsartWriteBusy(usrt)) {
-    //   }
   }
 
-  // for (uint8_t i = 0; i < 5; i++) {
-  //   MCCMasterSendAddress(rs, 94);
-  //   while (RS485Busy(rs)) {
-  //   }
-  //   RS485Write(rs, "a");
-  //   while (RS485Busy(rs)) {
-  //   }
-
-  //   String tbuf = MCCRead(rs);
-  //   // RS485Read(rs, buf);
-  //   memcpy(buf, tbuf.str, tbuf.len);
-
-  //   RS485MasterSendAddress(rs, 38);
-  //   while (RS485Busy(rs)) {
-  //   }
-  //   RS485Write(rs, "a");
-  //   while (RS485Busy(rs)) {
-  //   }
-  //   char a[100];
-  //   // RS485Read(rs, a);
-  //   String sbuf = MCCRead(rs);
-  //   // tbuf.str[tbuf.len] = '\0';
-  //   // strcat(buf, a);
-  //   memcpy(&buf[tbuf.len], sbuf.str, sbuf.len);
-
-  //   UsartWriteChars(usrt, sbuf.str, sbuf.len);
-  //   while (UsartWriteBusy(usrt)) {
-  //   }
-  // }
-
-  // while (UsartReadChar(usrt) == -1) {
-  // }
-  // }
   return 0;
 }
